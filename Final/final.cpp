fc3134977333b5b8fe9f0a5275e2f3ec1c71273f
Mario Bravo <mbravofuentes@mavs.coloradomesa.edu>
2019-12-08 19:02:17 -0700

    FinalSubmission

--- Final/final.cpp
+++ Final/final.cpp
@@ -1,6 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[1000000], n;
+int n;
+
+struct Node
+{
+    int data;
+    Node* left, * right;
+};
+Node* newNode(int data)
+{
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return (node);
+}
+Node* insertLevelOrder(vector<int>arr, Node* root, int i, int count)
+{
+    // Base case for recursion
+    if (i < count)
+    {
+        Node* temp = newNode(arr[i]);
+        root = temp;
+
+        // insert left child
+        root->left = insertLevelOrder(arr,
+                   root->left, 2 * i + 1, count);
+
+        // insert right child
+        root->right = insertLevelOrder(arr,
+                  root->right, 2 * i + 2, count);
+    }
+    return root;
+}
+void inOrder(Node* root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        cout << root->data <<" ";
+        inOrder(root->right);
+    }
+}
+bool ifNodeExists(struct Node* node, int x)
+{
+    if (node == NULL)
+        return false;
+
+    if (node->data == x)
+        return true;
+
+    /* then recur on left sutree */
+    bool res1 = ifNodeExists(node->left, x);
+
+    /* now recur on right subtree */
+    bool res2 = ifNodeExists(node->right, x);
+
+    return res1 || res2;
+}
 
 void swap(int *xp, int *yp)
 {
@@ -8,8 +64,7 @@ void swap(int *xp, int *yp)
     *xp = *yp;
     *yp = temp;
 }
-
-void bubbleSort(int arr[], int count)
+void bubbleSort(vector<int>arr, int count)
 {
     int i, j;
     for (i = 0; i < count-1; i++)
@@ -20,7 +75,7 @@ void bubbleSort(int arr[], int count)
             swap(&arr[j], &arr[j+1]);
 }
 
-void selectionSort(int arr[], int count)
+void selectionSort(vector<int>arr, int count)
 {
   int i, j, min_idx;
     for (i = 0; i < count-1; i++)
@@ -36,7 +91,7 @@ void selectionSort(int arr[], int count)
     }
 }
 
-void insertionSort(int arr[], int count)
+void insertionSort(vector<int>arr, int count)
 {
     int i, key, j;
     for (i = 1; i < count; i++)
@@ -56,7 +111,7 @@ void insertionSort(int arr[], int count)
     }
 }
 
-int shellSort(int arr[], int count)
+int shellSort(vector<int>arr, int count)
 {
     // Start with a big gap, then reduce the gap
     for (int gap = count/2; gap > 0; gap /= 2)
@@ -84,7 +139,7 @@ int shellSort(int arr[], int count)
     return 0;
 }
 
-int partition (int arr[], int low, int high)
+int partition (vector<int>arr, int low, int high)
 {
     int pivot = arr[high]; // pivot
     int i = (low - 1); // Index of smaller element
@@ -101,7 +156,7 @@ int partition (int arr[], int low, int high)
     swap(&arr[i + 1], &arr[high]);
     return (i + 1);
 }
-void quickSort(int arr[], int low, int high)
+void quickSort(vector<int>arr, int low, int high)
 {
     if (low < high)
     {
@@ -116,7 +171,7 @@ void quickSort(int arr[], int low, int high)
     }
 }
 
-void merge(int arr[], int l, int m, int r)
+void merge(vector<int>arr, int l, int m, int r)
 {
     int i, j, k;
     int n1 = m - l + 1;
@@ -168,7 +223,7 @@ void merge(int arr[], int l, int m, int r)
         k++;
     }
 }
-void mergeSort(int arr[], int l, int r)
+void mergeSort(vector<int>arr, int l, int r)
 {
     if (l < r)
     {
@@ -184,7 +239,7 @@ void mergeSort(int arr[], int l, int r)
     }
 }
 
-void heapify(int arr[], int n, int i)
+void heapify(vector<int>arr, int n, int i)
 {
     int largest = i; // Initialize largest as root
     int l = 2*i + 1; // left = 2*i + 1
@@ -207,7 +262,7 @@ void heapify(int arr[], int n, int i)
         heapify(arr, n, largest);
     }
 }
-void heapSort(int arr[], int count)
+void heapSort(vector<int>arr, int count)
 {
     for (int i = count / 2 - 1; i >= 0; i--)
         heapify(arr, count, i);
@@ -218,136 +273,327 @@ void heapSort(int arr[], int count)
   }
 }
 
-void printArray(int arr[], int count)
+void printArray(vector<int>arr, int count)
 {
   ofstream myfile("rand.txt");
   if (myfile.is_open())
   {
     for (int i=0; i<count; i++)
-    {
-    myfile << arr[i] << " ";
-    }
-  myfile.close();
+      myfile << arr[i] << " ";
+        myfile.close();
   }
   else
   {
-  cout << "File could not be opened" << endl;
+     cout << "File could not be opened" << endl;
   }
-}
-void readArray()
-{
-  string STRING;
- ifstream myfile;
- myfile.open ("rand.txt");
- while(getline(myfile,STRING))
- {
-   cout << "Reading completed " << endl;
-  }
- myfile.close();
-}
 
-void heapTime(int lower, int upper, int count)
+}
+void heapTime(vector<int>arr, int lower, int upper, int count, ofstream& a)
 {
-  readArray();
+  float sum = 0, avg;
+  for (int i = 0; i < 10; i++)
+  {
   auto t1 = std::chrono::high_resolution_clock::now();
   heapSort(arr, count);
   auto t2 = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>( t2 - t1 ).count();
-  cout << "HeapSort duration " << duration << " seconds" << endl;
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+  cout << "HeapSort " << duration << endl;
+  sum += duration;
+  }
+  avg = sum / 10;
+  a << "HEAPSORT_AVG " << avg << " milliseconds " << endl;
 }
-void bubbleTime(int lower, int upper, int count)
+void bubbleTime(vector<int>arr, int lower, int upper, int count, ofstream& a)
 {
-  readArray();
+  float sum = 0, avg;
+  for (int i = 0; i < 10; i++)
+  {
   auto t1 = std::chrono::high_resolution_clock::now();
   bubbleSort(arr, count);
   auto t2 = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>( t2 - t1 ).count();
-  cout << "BubbleSort duration " << duration << " seconds" << endl;
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+  cout << "BubbleSort " << duration << endl;
+  sum += duration;
 }
-void selectionTime(int lower, int upper, int count)
+avg = sum / 10;
+a << "BubbleSort_AVG " << avg << " milliseconds " << endl;
+}
+void selectionTime(vector<int>arr, int lower, int upper, int count, ofstream& a)
 {
-  readArray();
+  float sum = 0, avg;
+  for (int i = 0; i < 10; i++)
+  {
   auto t1 = std::chrono::high_resolution_clock::now();
   selectionSort(arr, count);
   auto t2 = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>( t2 - t1 ).count();
-  cout << "SelectionSort duration " << duration << " seconds" << endl;
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+  cout << "SelectionSort " << duration << endl;
+  sum += duration;
 }
-void insertionTime(int lower, int upper, int count)
+avg = sum / 10;
+a << "SelectionSort_AVG " << avg << " milliseconds " << endl;
+}
+void insertionTime(vector<int>arr, int lower, int upper, int count, ofstream& a)
 {
-  readArray();
+  float sum = 0, avg;
+  for (int i = 0; i < 10; i++)
+  {
   auto t1 = std::chrono::high_resolution_clock::now();
   insertionSort(arr, count);
   auto t2 = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>( t2 - t1 ).count();
-  cout << "InsertionSort duration " << duration << " seconds" << endl;
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+  cout << "InsertionSort " << duration << endl;
+  sum += duration;
+  }
+  avg = sum / 10;
+  a << "InsertionSort_AVG " << avg << " milliseconds " << endl;
 }
-void mergeTime(int lower, int upper, int count)
+void mergeTime(vector<int>arr, int lower, int upper, int count, ofstream& a)
 {
-  readArray();
+  float sum = 0, avg;
+  for (int i = 0; i < 10; i++)
+  {
   auto t1 = std::chrono::high_resolution_clock::now();
   mergeSort(arr, 0, count - 1);
   auto t2 = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>( t2 - t1 ).count();
-  cout << "MergeSort duration " << duration << " seconds" << endl;
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+  cout << "MergeSort " << duration << endl;
+  sum += duration;
 }
-void shellTime(int lower, int upper, int count)
+  avg = sum / 10;
+  a << "MergeSort_Avg " << avg << " milliseconds " << endl;
+}
+void shellTime(vector<int>arr, int lower, int upper, int count, ofstream& a)
 {
-  readArray();
+  float sum = 0, avg;
+  for (int i = 0; i < 10; i++)
+  {
   auto t1 = std::chrono::high_resolution_clock::now();
   shellSort(arr, count);
   auto t2 = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>( t2 - t1 ).count();
-  cout << "ShellSort duration " << duration << " seconds" << endl;
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+  cout << "ShellSort " << duration << endl;
+  sum += duration;
 }
-void quickTime(int lower, int upper, int count)
+  avg = sum / 10;
+  a << "ShellSort_Avg " << avg << " milliseconds " << endl;
+}
+void quickTime(vector<int>arr, int lower, int upper, int count, ofstream& a)
 {
-  readArray();
+  float sum = 0, avg;
+  for (int i = 0; i < 10; i++)
+  {
   auto t1 = std::chrono::high_resolution_clock::now();
   quickSort(arr, 0, count - 1);
   auto t2 = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>( t2 - t1 ).count();
-  cout << "QuickSort duration " << duration << " seconds" << endl;
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+  cout << "QuickSort " << duration << endl;
+  sum += duration;
 }
-void defaultSort(int lower, int upper, int count)
-{
-  readArray();
-  auto t1 = std::chrono::high_resolution_clock::now();
-  sort(arr, arr+count);
-  auto t2 = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>( t2 - t1 ).count();
-  cout << "Default Sort duration " << duration << " seconds" << endl;
+  avg = sum / 10;
+  a << "QuickSort_Avg " << avg << " milliseconds " << endl;
 }
-void stableSort(int lower, int upper, int count)
+void defaultSort(vector<int>arr, int lower, int upper, int count, ofstream& a)
 {
-  readArray();
+  float sum = 0, avg;
+  for (int i = 0; i < 10; i++)
+  {
   auto t1 = std::chrono::high_resolution_clock::now();
-  stable_sort(arr, arr + count);
+  sort(arr.begin(), arr.end());
   auto t2 = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>( t2 - t1 ).count();
-  cout << "Stable Sort duration " << duration << " seconds" << endl;
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+  cout << "DefaultSort " << duration << endl;
+  sum += duration;
+}
+avg = sum / 10;
+a << "DefaultSort " << avg << " milliseconds " << endl;
+}
+void stableSort(vector<int>arr, int lower, int upper, int count, ofstream& a)
+{
+  float sum = 0, avg;
+  for (int i = 0; i < 10; i++)
+  {
+  auto t1 = std::chrono::high_resolution_clock::now();
+  stable_sort(arr.begin(), arr.end());
+  auto t2 = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+  cout << "StableSort " << duration << endl;
+  sum += duration;
+}
+  avg = sum / 10;
+  a << "StableSort_Avg " << avg << " milliseconds " << endl;
+}
+int sequentialSearch(vector<int>arr, int count, ofstream& a)
+{
+  float sum = 0, avg;
+  for (int i = 0; i < 10; i++)
+  {
+  int key;
+  bool found = false;
+  cout << "Enter the value to search: ";
+  cin >> key;
+  auto t1 = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < count; i++) {
+       if (key == arr[i]) {
+           found = true;
+           cout << "The value is found at index arr[" << i << "]" << endl;
+       }
+   }
+   if (!found)
+   {
+       cout << "Key not found!";
+   }
+   auto t2 = std::chrono::high_resolution_clock::now();
+   auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+   cout << "SequentialSearch " << duration << endl;
+   sum += duration;
+ }
+ avg = sum / 10;
+ a << "SequentialSearch " << avg << " milliseconds " << endl;
+}
+int binaryS(vector<int>arr, int count, ofstream& a)
+{
+  float sum = 0, avg;
+  for (int i = 0; i < 10; i++)
+  {
+  int x;
+  cout << "What do you wanna search for? " << endl;
+  cin >> x;
+  auto t1 = std::chrono::high_resolution_clock::now();
+  if (binary_search(arr.begin(), arr.end(), x))
+          cout << "Element found in the array" << endl;
+      else
+          cout << "Element not found in the array" << endl;
+      auto t2 = std::chrono::high_resolution_clock::now();
+      auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+      cout << "BinarySearch " << duration << endl;
+      sum += duration;
+    }
+    avg = sum / 10;
+    a << "BinarySearchAVG " << avg << " milliseconds " << endl;
+}
+void BSTfind(vector<int>arr, int count, int x, ofstream& a)
+{
+  float sum = 0, avg;
+  for (int i = 0; i < 10; i++)
+  {
+  Node* root = insertLevelOrder(arr, root, 0, count);
+  cout << "What you looking for? " << endl;
+  cin >> x;
+  auto t1 = std::chrono::high_resolution_clock::now();
+  if(ifNodeExists(root, x))
+      cout << "Number found" << endl;
+    else
+      cout << "Number not in array" << endl;
+      auto t2 = std::chrono::high_resolution_clock::now();
+      auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+      cout << "BSTSearch " << duration << endl;
+      sum += duration;
+    }
+    avg = sum / 10;
+    a << "BSTFind " << avg << " milliseconds " << endl;
 }
 
-int main()
+void hundredruns(vector<int>arr, ofstream& a)
 {
-  int lower = 1, upper = 1000000, count = 100000;
+  int lower = 1, upper = 1000000, count = 100000, x;
     srand(time(0));
-    for (int i = 0; i < count; i++)
+    for (int i = 1; i < count; i++)
   {
         int num = (rand() %
             (upper - lower + 1)) + lower;
-          arr[i] = num;
+          arr.push_back(num);
   }
 printArray(arr, count);
-bubbleTime(lower, upper, count);
-/*quickTime(lower, upper, count);
-shellTime(lower, upper, count);
-mergeTime(lower, upper, count);
-heapTime(lower, upper, count);
-selectionTime(lower, upper, count);
-insertionTime(lower, upper, count);
-bubbleTime(lower, upper, count);
-defaultSort(lower, upper, count);
-stableSort(lower, upper, count);*/
+BSTfind(arr, count, x, a);
+quickTime(arr, lower, upper, count, a);
+shellTime(arr, lower, upper, count, a);
+mergeTime(arr, lower, upper, count, a);
+heapTime(arr, lower, upper, count, a);
+selectionTime(arr, lower, upper, count, a);
+insertionTime(arr, lower, upper, count, a);
+defaultSort(arr, lower, upper, count, a);
+stableSort(arr, lower, upper, count, a);
+binaryS(arr, count, a);
+sequentialSearch(arr, count, a);
+bubbleTime(arr, lower, upper, count, a);
+
+}
+void fivehundredruns(vector<int>arr, ofstream& a)
+{
+  int lower = 1, upper = 1000000, count = 500000, x;
+    srand(time(0));
+    for (int i = 1; i < count; i++)
+  {
+        int num = (rand() %
+            (upper - lower + 1)) + lower;
+          arr.push_back(num);
+  }
+printArray(arr, count);
+BSTfind(arr, count, x, a);
+quickTime(arr, lower, upper, count, a);
+shellTime(arr, lower, upper, count, a);
+mergeTime(arr, lower, upper, count, a);
+heapTime(arr, lower, upper, count, a);
+selectionTime(arr, lower, upper, count, a);
+insertionTime(arr, lower, upper, count, a);
+defaultSort(arr, lower, upper, count, a);
+stableSort(arr, lower, upper, count, a);
+binaryS(arr, count, a);
+sequentialSearch(arr, count, a);
+bubbleTime(arr, lower, upper, count, a);
+}
+void onemilruns(vector<int>arr, ofstream& a)
+{
+  int lower = 1, upper = 1000000, count = 1000000, x;
+    srand(time(0));
+    for (int i = 1; i < count; i++)
+  {
+        int num = (rand() %
+            (upper - lower + 1)) + lower;
+          arr.push_back(num);
+  }
+printArray(arr, count);
+BSTfind(arr, count, x, a);
+quickTime(arr, lower, upper, count, a);
+shellTime(arr, lower, upper, count, a);
+mergeTime(arr, lower, upper, count, a);
+heapTime(arr, lower, upper, count, a);
+selectionTime(arr, lower, upper, count, a);
+insertionTime(arr, lower, upper, count, a);
+defaultSort(arr, lower, upper, count, a);
+stableSort(arr, lower, upper, count, a);
+binaryS(arr, count, a);
+sequentialSearch(arr, count, a);
+bubbleTime(arr, lower, upper, count, a);
+}
+
+
+int main()
+{
+  vector<int>arr;
+  ofstream a("timecomplexity.txt");
+  a.is_open();
+  a << "First 100000 numbers " << endl;
+  hundredruns(arr, a);
+  a << "Second 500000 numbers " << endl;
+  fivehundredruns(arr, a);
+  a << "Third 1000000 numbers " << endl;
+  onemilruns(arr, a);
+  /*vector<double>arr2;
+  vector<string>arr3;
+  ifstream file("timecomplexity.txt");
+  if (file) {
+    double value;
+    string wri;
+    while ( file >> wri >> value ) {
+      arr3.push_back(wri);
+        arr2.push_back(value);
+    }
+}
+/*sort(arr2.begin(), arr2.end(), std::greater<>());
+a << "These are how the Avg Times went in milliseconds " << endl;
+a << "The numbers go descend from 1.BST Search, 2"*/
+a.close();
 return 0;
 }
